55217ed1b81a3c61ab5cd5d17899be39ab7673b6
silverdiva <silverdiva4u@gmail.com>
2019-03-25 22:11:05 -0700

    moved all files, folders to heroku might-chamber sub-dir.

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require("express");
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
 
 
